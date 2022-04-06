df3f0a084faa4600b13b0a49f8b6a6a18ee6309a
refactory <engineering@refactory.com>
2022-04-06 20:53:12 +0700

    imageSize

--- src/SwipeRating.tsx
+++ src/SwipeRating.tsx
@@ -347,9 +347,9 @@ export default class SwipeRating extends Component<
     const width = position.x.interpolate(
       {
         inputRange: [
-          -ratingCount * ( imageSize / 2 ),
+          -ratingCount * imageSize,
           0,
-          ratingCount * ( imageSize / 2 )
+          ratingCount * imageSize
         ],
         outputRange: [
           ratingCount * imageSize,
