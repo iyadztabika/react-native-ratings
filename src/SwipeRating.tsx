73cf951c953a3d18d73e50c2da15de8c3cde4d3d
refactory <engineering@refactory.com>
2022-04-07 15:52:45 +0700

    Revert "left starsWrapper"
    This reverts commit c7325995eb79f03cb2ef493dd593b54013f77e19.

--- src/SwipeRating.tsx
+++ src/SwipeRating.tsx
@@ -544,7 +544,7 @@ const styles = StyleSheet.create( {
     position: "absolute",
     top: 0,
     left: 0,
-    right: 20,
+    right: 0,
     bottom: 0,
     flexDirection: "row",
     justifyContent: "center",
