bcd05571dc45039035e40afcc7a1389c8952e562
refactory <engineering@refactory.com>
2022-04-07 09:01:05 +0700

    imageSize 6

--- dist/SwipeRating.js
+++ dist/SwipeRating.js
@@ -146,9 +146,9 @@ export default class SwipeRating extends Component {
         const { backgroundColor } = TYPES[type];
         const width = position.x.interpolate({
             inputRange: [
-                -ratingCount * (imageSize * 2),
+                -ratingCount * (imageSize / 2),
                 0,
-                ratingCount * (imageSize * 2)
+                ratingCount * (imageSize / 2)
             ],
             outputRange: [
                 ratingCount * imageSize,
